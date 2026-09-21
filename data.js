616bca65112c6e519a05c071f2895e882f0418ca
Matthew Brown <mattbrown67@gmail.com>
2026-09-21 15:27:29 -0400

    textbook solutions day 1

--- data.js
+++ data.js
@@ -26,7 +26,7 @@
 
    To SAVE TIME:
 
-   git add . && git commit -m "textbook day 1" && git push origin main
+   git add . && git commit -m "textbook solutions day 1" && git push origin main
 
    ═══════════════════════════════════════════════════════════ */
 
@@ -56,8 +56,10 @@ const COURSE = {
       note:D("1Vvs50UzRnrbaqEv-CZfI-na-WLJXTFIi"), ans:"https://drive.google.com/file/d/1ptPs7517HfxPLUSGRumwIvKYi1BivJMZ/view", extras:[
       ["Unit 1 Prerequisite Skills (answers included)",D("1BLqvIx4vk7_WPuIeWRkOX_5tSTBoI7Ul"),"skills"],
       ["Textbook","https://drive.google.com/file/d/1PL1poX-gGXle8lF6WQp6sckQJbnAVbBZ/view?usp=drive_link","doc"],
-      ["Corrections","https://drive.google.com/file/d/104AH3b9ihj_jlsRTZwswXUSc9O5Ufywm/view?usp=drive_link","ans"],
-      ["LG/SC Review from 3U",D("1B4y9b2TMDEZYqG_tXoG2RCuPREQPI9ef"),"doc"],,
+      ["Textbook Corrections","https://drive.google.com/file/d/104AH3b9ihj_jlsRTZwswXUSc9O5Ufywm/view?usp=drive_link","ans"],
+      ["Textbook (separate files for each chapter","https://drive.google.com/drive/folders/1iKDpCSdqUBvsYqEQ8D0wPoLW2ZyOfTHw?usp=drive_link","doc"],
+      ["Textbook Solutions","https://drive.google.com/drive/folders/1Khpcx1R0LTrgbSWFfU-ICRGR8TYOcQ6j?usp=drive_link","doc"],
+      ["LG/SC Review from 3U",D("1B4y9b2TMDEZYqG_tXoG2RCuPREQPI9ef"),"doc"],
       ["Domain & Range Worksheet (answers included)",D("12D6Wf_aWjvp3TMwibvPRFKc4oUhs6nWw"),"doc"],
       ["Domain & Range of Graphs","https://www.youtube.com/watch?v=YjVc9ookvRs","vid"],
     ]},
